dd875806910d1c23fa3c5f432cfc5a122d9fc47f
Kein Pyi Si <keinpyisi@gmail.com>
2022-12-07 07:55:37 +0900

    Auto stash before checking out "origin/htut"

---
+++ commands/startup/query_check.js
@@ -0,0 +1,63 @@
+/**
+ * @file Sample ping command
+ * @author Naman Vrati
+ * @since 1.0.0
+ * @version 3.2.2
+ */
+
+/**
+ * @type {import('../../typings').LegacyCommand}
+ */
+ const { dirname } = require('path');
+ const appDir = dirname(require.main.filename);
+ var Utality = require(appDir+'/utality/utality');
+ var Query = require(appDir+'/utality/query');
+
+module.exports = {
+	name: 'querycheck',
+	// Refer to typings.d.ts for available properties.
+
+	execute (message, args) {
+		Utality.Log(message)
+		Utality.Log(args)
+		try{
+			var con = require(appDir+'/utality/connection');
+		
+			Utality.Log("Connected");
+			con.getConnection(function(err, conn) {
+				
+				function queryData() {
+					var sql_select = Query.all_terrain_in_land;
+					//World SQL
+					con.query(sql_select, function (err, result) {
+					
+						if (err) throw err 
+						result.map(Query =>{
+                            Utality.Log(Query)
+							// var json = {"Resources ": ResourceName.resource_name};
+							Utality.Embed(message,Query,"Query Data List","Query Details");
+						});
+							
+				});
+			}
+			
+				function releaseQuery() {
+					Utality.Log("Log Out")
+					// return the query back to the pool
+					conn.release();
+				}
+			
+				queryData();
+				releaseQuery();
+				Utality.Log(`All Connections ${con._allConnections.length}`);
+				Utality.Log(`Acquiring Connections ${con._acquiringConnections.length}`);
+				Utality.Log(`Free Connections ${con._freeConnections.length}`);
+				Utality.Log(`Queue Connections ${con._connectionQueue.length}`);
+			});
+		
+					
+		}catch(e){
+			Utality.Log(e);
+		}
+	}
+}
--- utality/query.js
+++ utality/query.js
@@ -1,8 +1,8 @@
-const all_land='SELECT * FROM land'
+const all_land='SELECT * FROM land ln JOIN `world` w ON ln.world_id = w.id'
 const insert_land='INSERT INTO land (land_name, world_id) VALUES (?,?)'
-const select_land_world='SELECT * FROM land where world_id= ?'
+const select_land_world='SELECT * FROM land ln JOIN `world` w ON ln.world_id = w.id where world_id= ?'
 
-const all_resource='SELECT * FROM resources'
+const all_resource='SELECT * FROM resources rs JOIN `world` w ON rs.world_id = w.id'
 const insert_resource='INSERT INTO resources (world_id, resource_name, resource_quantity, fixed_amount) VALUES (?, ?, ?, ?)'
 
 const all_terrain='SELECT * FROM terrain'
@@ -14,7 +14,20 @@ const insert_world='INSERT INTO world (world_name) VALUES (?)'
 
 const insert_channel='INSERT INTO channels (server_id,channel_name,channel_id,terrain_id,land_id,world_id) VALUES (?,?,?,?,?,?)'
 
+const select_terrainland_linked='SELECT * FROM terrain_in_land til JOIN `terrain` t ON til.terrain_id=t.id JOIN `land` l ON l.id = til.land_id JOIN `world` w ON l.world_id = w.id ORDER BY t.terrain_name ASC'
 const insert_terrain_land='INSERT INTO terrain_in_land (terrain_id,land_id) VALUES (?,?)';
+
+const select_terrain_event='SELECT * FROM terrain_event where terrain_in_land_id = ?'
+
+const select_all_channel='SELECT * FROM channels cha JOIN `terrain` t ON cha.terrain_id=t.id JOIN `land` l ON l.id = cha.land_id JOIN `world` w ON cha.world_id = w.id ORDER BY t.terrain_name ASC'
+const select_terrain_channel = 'SELECT * FROM channels cha JOIN `terrain` t ON cha.terrain_id=t.id JOIN `land` l ON l.id = cha.land_id JOIN `world` w ON cha.world_id = w.id where cha.terrain_id= ?'
+const select_land_channel='SELECT * FROM channels cha JOIN `terrain` t ON cha.terrain_id=t.id JOIN `land` l ON l.id = cha.land_id JOIN `world` w ON cha.world_id = w.id where cha.land_id= ?'
+const select_world_channel='SELECT * FROM channels cha JOIN `terrain` t ON cha.terrain_id=t.id JOIN `land` l ON l.id = cha.land_id JOIN `world` w ON cha.world_id = w.id where cha.world_id= ?'
+const select_channel_id='SELECT * FROM channels cha JOIN `terrain` t ON cha.terrain_id=t.id JOIN `land` l ON l.id = cha.land_id JOIN `world` w ON cha.world_id = w.id where cha.channel_id= ?'
+
+const select_resource_in_terrain_land='SELECT * FROM resource_in_terrain_land ritl JOIN terrain_in_land tl ON ritl.terrain_in_land_id = tl.id JOIN resources rs ON ritl.resource_id = rs.id JOIN terrain t ON tl.terrain_id = t.id JOIN land lt ON tl.land_id = lt.id JOIN world w ON w.id = rs.world_id'
+
+
 module.exports = { 
     all_land, 
     select_world_name,
@@ -27,6 +40,15 @@ module.exports = {
     insert_world,
     insert_channel,
     insert_terrain_land,
-    select_land_world
+    select_land_world,
+    select_terrainland_linked,
+    select_terrain_event,
+    select_all_channel,
+    select_terrain_channel,
+    select_land_channel,
+    select_world_channel,
+    select_channel_id,
+    select_resource_in_terrain_land
+
 
 }
